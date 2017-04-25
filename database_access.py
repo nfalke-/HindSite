6580f1bd13a0abf4de9fcad28f0f009d41c32d6f
nfalke- <nate.falke@gmail.com>
2017-04-24 23:16:15 -0500

    added more functions to get stuff from the db, commit messages will get better, I swear

--- database_access.py
+++ database_access.py
@@ -52,6 +52,26 @@ def get_steps_for_test(testid):
         stepnumber'''
     return get_from_db(query, (testid))
 
+def list_suites():
+    query = '''
+    select
+        suitename, suiteid
+    from
+        suites
+    '''
+    return get_from_db(query)
+
+def list_tests(suite_id):
+    query = '''
+    select
+        testname, testid
+    from
+        tests
+    where
+        suiteid = %s
+    '''
+    return get_from_db(query, (suite_id, ))
+
 def add_steps_to_test(testid, steps):
     delete_steps_from_test(testid)
     query = '''insert into
@@ -59,5 +79,37 @@ def add_steps_to_test(testid, steps):
     values
         (%s, %s, %s, %s)'''
     steps = tuple((testid, stepnumber, step[0], step[1]) for stepnumber, step in enumerate(steps, 1))
+    print(steps)
     write_many_to_db(query, steps)
 
+def add_suite(name):
+    query = """insert into
+        suites (suitename)
+    values
+        (%s)"""
+    write_to_db(query, name)
+
+def add_test(suiteid, name):
+    query = """insert into
+        tests(testname, suiteid)
+    values
+        (%s, %s)"""
+    write_to_db(query, (name, suiteid))
+
+def add_run(testid, runtime):
+    query = """insert into
+        previousruns(testid, runtime)
+    values
+        (%s, %s)"""
+    write_to_db(query, (testid, runtime))
+
+def get_run_by_id(runid):
+    query = '''
+    select
+        testid, runtime
+    from
+        previousruns
+    where
+        runid = %s
+    '''
+    return get_from_db(query, (runid))[0]
