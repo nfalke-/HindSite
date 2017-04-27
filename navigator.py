bdaf7252ed378256835e94a42afbd4c48886e797
nfalke- <nate.falke@gmail.com>
2017-04-27 07:53:58 -0500

    added pyvirtualdisplay, trying and failing to get video to the right size

--- navigator.py
+++ navigator.py
@@ -4,18 +4,26 @@ from PIL import Image, ImageChops, ImageDraw
 from io import StringIO, BytesIO
 import os
 from camera import Video
+from pyvirtualdisplay import Display
 
 base = './static'
+WIDTH=3935
+HEIGHT=1080
 
 class Navigator(object):
     def __init__(self, task_list, test_id, wait_time=5, browser=None):
+        self.vdisplay = Display(backend='xvfb', size=(WIDTH, HEIGHT))
+        self.vdisplay.start()
         if not browser:
-            browser = webdriver.Firefox()
+            browser = webdriver.Chrome()
         self.runtime = str(int(time() * 1000))
         self.directory = os.path.join(base, str(test_id), self.runtime)
         if not os.path.isdir(self.directory):
             os.makedirs(self.directory)
         self.browser = browser
+        self.browser.maximize_window()
+        #self.browser.set_window_size(WIDTH, HEIGHT)
+        #self.browser.set_window_position(0, 0)
         self.wait_time = wait_time
         self.task_list = task_list
         self.full_screenshots = []
@@ -23,7 +31,8 @@ class Navigator(object):
             'visit': self._visit,
             'click': self._click,
         }
-        self.video = Video(os.path.join(self.directory, "video.mp4"), 0, 0, "1280", "1024")
+        self.video = Video(os.path.join(self.directory, "video"), 0, 0, WIDTH, HEIGHT)
+        self.image_no = 0
 
 
     def _diff_screenshots(self, a, b, opacity=0.85):
@@ -49,8 +58,10 @@ class Navigator(object):
     def _take_fullpage_screenshot(self):
         total_width = self.browser.execute_script("return document.body.offsetWidth")
         total_height = self.browser.execute_script("return document.body.parentNode.scrollHeight")
-        client_width = self.browser.execute_script("return document.body.clientWidth")
-        client_height = self.browser.execute_script("return window.innerHeight")
+        partial_screenshot = self._take_partial_screenshot()
+        client_width, client_height = partial_screenshot.size
+        total_width = max(total_width, client_width)
+        total_height = max(total_height, client_height)
         full_screenshot = Image.new('RGBA', (total_width, total_height))
 
         x, y = 0, 0
@@ -64,7 +75,6 @@ class Navigator(object):
             x = 0
             y += client_height
         self.browser.execute_script("window.scrollTo({0}, {1})".format(0, 0))
-        print("window.scrollTo({0}, {1})".format(0, 0))
         #topnav = self.browser.find_element_by_tag_name("header")
         #self.browser.execute_script("arguments[0].setAttribute('style', 'position: absolute; top: 0px;')", topnav)
         return full_screenshot
@@ -75,7 +85,7 @@ class Navigator(object):
     def _visit(self, url):
         self.browser.get(url)
         self.video.resume()
-        sleep(7)
+        sleep(2)
         self.video.pause()
 
     def _click(self, css_selector):
@@ -85,10 +95,13 @@ class Navigator(object):
         self.video.pause()
         element.click()
         self.video.resume()
-        sleep(5)
+        sleep(2)
         self.video.pause()
 
     def _do_task(self, task):
+        self.image_no += 1
+        filename = os.path.join(self.directory, "{}.png".format(self.image_no))
+        self.browser.save_screenshot(filename)
         sleep(2)
         self.commands[task[0]](task[1])
         sleep(self.wait_time)
@@ -101,11 +114,11 @@ class Navigator(object):
             driver.execute_script("arguments[0].setAttribute('style', arguments[1]);",
                                   element, s)
         original_style = element.get_attribute('style')
-        sleep(1)
+        sleep(.5)
         apply_style("background: yellow; border: 2px solid red;")
         sleep(1)
         apply_style(original_style)
-        sleep(1)
+        sleep(.5)
 
     def run(self):
         start = time()
@@ -113,17 +126,18 @@ class Navigator(object):
             self._do_task(task)
         self.video.resume()
         self.video.stop()
-        print(time() - start)
         self.browser.close()
         for i, screenshot in enumerate(self.full_screenshots):
             #b = Image.open('full_screenshot{}.png'.format(i))
             #self._diff_screenshots(screenshot, b).save('diff{}.png'.format(i))
             screenshot.save(os.path.join(self.directory, 'full_screenshot{}.png'.format(i)))
+        self.vdisplay.stop()
         return self.runtime
 
 if __name__ == "__main__":
-    test_id = 5
-    task_list = (('visit', 'https://ubuntuforums.org/showthread.php?t=1451362'),)
+    test_id = 100
+    task_list = (('visit', 'https://docs.python.org/3/library/io.html'),
+                ('click', 'body > div.document > div.sphinxsidebar > div.sphinxsidebarwrapper > p:nth-child(6) > a'))
     n = Navigator(task_list, test_id)
     n.run()
 
