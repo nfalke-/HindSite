785efdc24fcf3d4d8af72e51fa3922efc3b95c40
nfalke- <nate.falke@gmail.com>
2017-04-27 07:51:30 -0500

    turned on ffmpeg processing and tweaked output

--- camera.py
+++ camera.py
@@ -5,16 +5,19 @@ import signal
 start = time()
 class Video(object):
     def __init__(self, output_file, xPos=None, yPos=None, width=None, height=None):
+        self.output_file = output_file
         recording_args = [str(i) for i in ['recordmydesktop',
             "-x", xPos,
             "-y", yPos,
             "--height", height,
             "--width", width,
-            "-o", output_file,
-            "--no-sound"
+            "-o", '{}.ogv'.format(self.output_file),
+            "--no-sound",
+            "--full-shots",
+            "--no-cursor"
         ]]
 
-        self.process = subprocess.Popen(recording_args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        self.process = subprocess.Popen(recording_args)#, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         self.process.send_signal(signal.SIGSTOP)
 
     def start(self):
@@ -28,3 +31,11 @@ class Video(object):
 
     def stop(self):
         self.process.send_signal(signal.SIGINT)
+        self.process.communicate()
+        command = [
+            'ffmpeg',
+            '-i', '{}.ogv'.format(self.output_file),
+            '-c:v', 'libx264',
+            '{}.mp4'.format(self.output_file)
+        ]
+        subprocess.Popen(command)#, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
